e91825f3dd3b72d129fb2db9d02e2bb75638e197
elgrendar <info@rafacampanero.es>
2025-03-17 19:59:57 +0100

    Se introduce la versión de la app y se modifica el footer para avisar en los servidores son de prueba si APP_DEBUG es true

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,6 +1,11 @@
 <footer class="bg-gray-200 text-center text-sm py-2 fixed inset-x-0 bottom-0 shadow">
+    <div class="mt-12 text-center">
+        <h2 class="text-xl font-bold text-red-600">
+            {{ env('APP_DEBUG') == true ? 'Este servidor es una demo y los datos se pueden borrar periodicamente' : '' }}</h2>
+    </div>
     <h3 class="font-semibold">👨‍💻 Desarrollado por
-        <a href="https://rafacampanero.es" target="_blank" class="text-blue-600">Rafa Campanero</a>
+        <a href="https://rafacampanero.es" target="_blank" class="text-blue-600">Rafa Campanero</a> Versión
+        {{ env('APP_VERSION') }}
     </h3>
     <p class="text-gray-500">
         Creando soluciones innovadoras para mejorar la productividad.
